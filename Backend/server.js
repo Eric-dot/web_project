a169a9e25e97e11da6f67892fe149e5e39513ecf
zqran <uuxnet@gmail.com>
2022-11-03 12:45:36 +0800

    feat: update deps

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,24 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const axios = require('axios')
+const cors = require('cors')
 
-const app = express();
-app.all('*', function (req, res, next) {
-    // res.header('Access-Control-Allow-Origin','http://localhost:3000'); // 允许某个地址
-    res.header('Access-Control-Allow-Origin', '*') //当允许携带cookies此处的白名单不能写’*’
-    res.header(
-        'Access-Control-Allow-Headers',
-        'content-type,Content-Length, Authorization,Origin,Accept,X-Requested-With'
-    ) // 允许的请求头
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT') // 允许的请求方法
-    res.header('Access-Control-Allow-Credentials', true) // 允许携带cookies
-    next()
-})
+const app = express()
 
-app.set("view engine", "ejs")
+// 处理跨域
+app.use(cors())
 
 app.use('/search', async (req, res) => {
-    const apiRes = await axios({
-        url: `https://api.yelp.com/v3/businesses/search`,
-        params: req.query,
-        headers: {
-            Authorization: req.headers.Authorization,
-        },
-    })
-    res.json(apiRes.data)
+  // console.log(req.header('Authorization'))
+  const apiRes = await axios({
+    url: `https://api.yelp.com/v3/businesses/search`,
+    params: req.query,
+    headers: {
+      Authorization: req.header('Authorization'),
+    },
+  })
+  res.json(apiRes.data)
 })
 
-// app.get("/",(req,res)=>{
-//     console.log("Hi")
-//     res.render("index")
-// })
-app.listen(8080);
+app.listen(8080)
--- frontend/react-task-tracker/src/App.js
+++ frontend/react-task-tracker/src/App.js
@@ -1,34 +1,26 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Header from './components/Header'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-     const name = "Brand"
+  useEffect(() => {
+    const axiosTask = async () => {
+      const res = await axios({
+        method: 'get',
+        url: 'http://localhost:8080/search',
+        params: {
+          quary: 'dinner',
+        },
+        headers: {
+          Authorization:
+            'Bearer pvwAzDkoQqbsC8dzksa9R3ScsLuerrVw8b3RC0YuAVPJnasUdK3CLCgWyFEIH1wFS2iqW1Ykx3z1wFGaYGgHyxhiJ4dVvTrgg5Kqr-yl3ju7fpm3OrkdjefoGj9jY3Yx',
+        },
+      })
 
-
-    useEffect(()=>{
-            const axiosTask = async()=>{
-                const res = await axios({
-                    method: 'get',
-                    url: 'http://localhost:8080/search',
-                    params:{
-                        quary:'dinner'
-                    },
-                    headers: {
-                        Authorization: 'Bearer pvwAzDkoQqbsC8dzksa9R3ScsLuerrVw8b3RC0YuAVPJnasUdK3CLCgWyFEIH1wFS2iqW1Ykx3z1wFGaYGgHyxhiJ4dVvTrgg5Kqr-yl3ju7fpm3OrkdjefoGj9jY3Yx'
-                    }
-                })
-
-                console.log(res)
-            }
-            axiosTask()
-        }
-    )
-    return (
-        <div>
-
-        </div>
-    );
+      console.log(res)
+    }
+    axiosTask()
+  })
+  return <div></div>
 }
 
-export default App;
+export default App
