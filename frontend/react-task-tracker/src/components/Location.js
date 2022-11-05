51320cdf46a7c13bbac66c99383eca300a51419c
Eric-dot <rongcanf@usc.edu>
2022-11-04 21:44:18 -0700

    Create Location.js

---
+++ frontend/react-task-tracker/src/components/Location.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Location = () => {
+    return (
+        <div className='row'>
+            <div className="col-4">
+            </div>
+            <div className="col-1 my-2">
+                <button type="button" className="btn btn-danger">Submit</button>
+
+            </div>
+            <div className="col-1 ms-5 my-2">
+
+                <button type="button" className="btn btn-primary">Clear</button>
+            </div>
+            <div className="col">
+            </div>
+
+        </div>
+    )
+}
+
+export default Location
